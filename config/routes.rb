51bf07737cbdb9ed2ca0a9c205238668cecc6113
bercik156 <bercik156@gmail.com>
2016-10-03 04:57:43 +0200

    1.2 Add comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   mount Ckeditor::Engine => '/ckeditor'
-  resources :posts
+
+
+  resources :posts do
+    resources :comments
+  end
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
